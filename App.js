2b72283155c52abd4f62a4c017655091912c7ee0
Todd H. Albert <todd@bocacode.com>
2023-11-07 15:43:21 -0500

    Goal is tracked

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
+import { useState } from 'react'
 import { GluestackUIProvider, Box } from '@gluestack-ui/themed'
 import { config } from '@gluestack-ui/config'
 import Title from './components/Title'
+import Goal from './components/Goal'
 import WaterImage from './components/WaterImage'
 
 export default function App() {
+
+  const [volume, setVolume] = useState(500)
+
   return (
     <GluestackUIProvider config={config}>
       <Box
@@ -13,6 +18,7 @@ export default function App() {
         alignItems='center'
         bgColor='$blue300'>
           <Title />
+          <Goal volume={volume} />
           <WaterImage />
       </Box>
     </GluestackUIProvider>
