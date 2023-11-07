26234e4e9716070b7b76a9300a7b491e06405d80
Todd H. Albert <todd@bocacode.com>
2023-11-07 16:20:13 -0500

    added reset button

--- components/Controls.jsx
+++ components/Controls.jsx
@@ -2,7 +2,7 @@ import { Button, ButtonText, Box } from "@gluestack-ui/themed";
 
 export default function Controls({ setVolume, volume }) {
   return (
-    <Box flexDirection="row" mt="$8">
+    <Box flexDirection="row" flexWrap="wrap" justifyContent="center" mt="$8">
 
       <Button m="$4" size="xl" rounded="$xl"
         onPress={() => setVolume(volume + 250)}>
@@ -12,6 +12,11 @@ export default function Controls({ setVolume, volume }) {
       <Button m="$4" size="xl" rounded="$xl"
         onPress={() => setVolume(volume + 750)}>
         <ButtonText>Add 🫙</ButtonText>
+      </Button>
+
+      <Button m="$4" size="xl" rounded="$xl" action="negative"
+        onPress={() => setVolume(0)}>
+        <ButtonText>Reset 🙅</ButtonText>
       </Button>
 
     </Box>
