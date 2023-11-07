acf42781cd90cae50746be27bc00904524ff8672
Todd H. Albert <todd@bocacode.com>
2023-11-07 15:00:35 -0500

    Title and image added

---
+++ components/WaterImage.jsx
@@ -0,0 +1,16 @@
+import { Box, Image } from "@gluestack-ui/themed";
+import drops from "../assets/splash-square.jpg";
+
+export default function WaterImage() {
+  return (
+    <Box rounded="$full">
+      <Image
+        source={drops}
+        borderColor="$white"
+        borderWidth={4}
+        alt="Water droplets on glass"
+        size="2xl"
+        rounded="$full" />
+    </Box>
+  )
+}
